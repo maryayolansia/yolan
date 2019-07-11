67c1d981926b47434970342af8f28ec8bd276d34
maryayolansia <39435495+maryayolansia@users.noreply.github.com>
2019-07-11 12:14:04 +0700

    Update proses_tambah.php

--- www/proses_tambah.php
+++ www/proses_tambah.php
@@ -8,7 +8,7 @@ $nis_siswa      = $_POST['nis_siswa'];
 $alamat_siswa   = $_POST['alamat_siswa'];
 $notelp_siswa   = $_POST['notelp_siswa'];
  
-$input          = mysqli_query($conn, "INSERT INTO data_siswa (id_siswa, nama_siswa, nis_siswa, alamat_siswa, notelp_siswa) VALUES ($id_siswa, '$nama_siswa', '$nis_siswa','$alamat_siswa' , '$notelp_siswa')")
+$input          = mysqli_query($conn, "INSERT INTO data_siswa (id_siswa, nama_siswa, nis_siswa, alamat_siswa, notelp_siswa) VALUES ('$id_siswa', '$nama_siswa', '$nis_siswa','$alamat_siswa' , '$notelp_siswa')");
  
 if($input){
     echo "<script>alert('Data Berhasil Ditambah');document.location.href='index.php'</script>";
