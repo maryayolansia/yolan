1f61b634694d0d533bbb7ea4cb3bc556cd804b18
maryayolansia <39435495+maryayolansia@users.noreply.github.com>
2019-07-11 13:45:28 +0700

    Update proses_edit.php

--- www/proses_edit.php
+++ www/proses_edit.php
@@ -8,7 +8,7 @@ $nis_siswa      = $_POST['nis_siswa'];
 $alamat_siswa   = $_POST['alamat_siswa'];
 $notelp_siswa   = $_POST['notelp_siswa'];
  
-$update         = mysqli_query($conn, "UPDATE data_siswa SET nama_siswa ='$id_siswa', '$nama_siswa', nis_siswa = '$nis_siswa', alamat_siswa = '$alamat_siswa', notelp_siswa = '$notelp_siswa' WHERE id_siswa = '$id_siswa'");
+$update         = mysqli_query($conn, "UPDATE data_siswa SET id_siswa = '$id_siswa', nama_siswa = '$nama_siswa', nis_siswa = '$nis_siswa', alamat_siswa = '$alamat_siswa', notelp_siswa = '$notelp_siswa' WHERE id_siswa = '$id_siswa'");
  
 if($update){
     echo "<script>alert('Data Berhasil Diedit');document.location.href='index.php'</script>";
